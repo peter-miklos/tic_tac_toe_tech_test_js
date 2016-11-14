1036ca743a707ba39dfb546ca4e33c5eb514a561
Peter Miklos <pt.miklos@gmail.com>
2016-11-14 14:58:08 +0000

    getGrid and claimField method and test cases added

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -2,8 +2,29 @@
 
 describe("Grid", function() {
   var grid;
+  var player1;
+  var player2;
 
   beforeEach(function() {
+    player1 = jasmine.createSpyObj("player1", ['getName']);
+    player2 = jasmine.createSpyObj("player2", ['getName']);
     grid = new Grid();
   })
+
+  describe("#claimField", function() {
+    it("add the player into the requested field", function() {
+      grid.claimField(player1, 0, 1);
+      expect(grid._gridContent[1][0]).toEqual(player1);
+    })
+  })
+
+  describe("#getGrid", function() {
+    it("returns a copy of the grid", function() {
+      grid.claimField(player2, 2, 2)
+      expect(grid.getGrid()[2][2]).toEqual(player2)
+    })
+  })
+
+
+
 })
--- src/Grid.js
+++ src/Grid.js
@@ -1,9 +1,16 @@
 "use strict"
 
-function Grid() {
-
+function Grid(gridSize) {
+  const _GRID_SIZE = 3
+  this._gridSize = gridSize ? gridSize : _GRID_SIZE
+  this._gridContent = new Array(this._gridSize).fill(new Array(this._gridSize))
 }
 
 Grid.prototype = {
-  
+  getGrid: function() {
+    return this._gridContent.copyWithin()
+  },
+  claimField: function(player, x, y) {
+    this._gridContent[y][x] = player;
+  }
 }
