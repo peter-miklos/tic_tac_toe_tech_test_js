dc01aad35b5b58fdb60010eb2454900f30056370
Peter Miklos <pt.miklos@gmail.com>
2016-11-14 14:12:41 +0000

    basic game related test cases added

---
+++ spec/GameSpec.js
@@ -0,0 +1,45 @@
+"use strict"
+
+describe("Game", function() {
+  var game;
+  var player1;
+  var player2;
+  var grid
+
+  beforeEach(function() {
+    player1 = jasmine.createSpyObj("player1", ["name"])
+    player2 = jasmine.createSpyObj("player2", ["name"])
+    grid = jasmine.createSpyObj("grid")
+    game = new Game(player1, player2, grid)
+  })
+
+  describe("#getPlayer1", function() {
+    it("returns player1", function() {
+      expect(game.getPlayer1()).toEqual(player1);
+    })
+  })
+
+  describe("#getPlayer2", function() {
+    it("return player2", function() {
+      expect(game.getPlayer2()).toEqual(player2);
+    })
+  })
+
+  describe("#getWinner", function() {
+    xit("returns the winner of the game", function() {
+
+    })
+  })
+
+  describe("#getGrid", function() {
+    xit("returns the current grid", function() {
+
+    })
+  })
+
+  describe("#play", function() {
+    
+  })
+
+
+})
